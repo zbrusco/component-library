f03001153577d4615560fe93096c82ad1c29236a
zbrusco <194739391+zbrusco@users.noreply.github.com>
2025-09-02 12:34:22 -0300

    feat: implement Menu component with context and toggle functionality

---
+++ components/Menu/MenuDropdown.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { MenuContext } from "./Menu";
+
+export default function MenuDropdown({ children }) {
+  const { open } = React.useContext(MenuContext);
+
+  return <>{open ? <div className="menu-dropdown">{children}</div> : null}</>;
+}
---
+++ components/Menu/MenuItem.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MenuItem({ children }) {
+  return <div className="menu-item">{children}</div>;
+}
