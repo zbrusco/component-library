1dd07cb3cb4c5fbf54dcb0ac1e73fb133d725f57
zbrusco <194739391+zbrusco@users.noreply.github.com>
2025-09-08 16:18:22 -0300

    feat: add Badge, Banner, Card, and Testimonial styles

--- index.jsx
+++ index.jsx
@@ -1,11 +1,53 @@
 import ReactDOM from "react-dom/client";
 import Menu from "./components/Menu/index";
 import Star from "./components/Star";
+import Badge from "./components/Badge";
+import Banner from "./components/Banner";
+import Card from "./components/Card";
+import Testimonial from "./components/Testimonials/Testimonial";
+import TestimonialNoPic from "./components/Testimonials/TestimonialNoPic";
+import Logo from "./static/companyLogo.png";
 
 function App() {
   return (
     <>
       <Star />
+      <br />
+      <Badge type="pill" color="indigo">
+        Badge
+      </Badge>
+      <Banner
+        status="success"
+        title="Congratulations!"
+        msg="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam."
+      />
+      <Banner title="Info" />
+      <Banner status="warning" title="Alert">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+        pariatur, ipsum similique veniam.
+      </Banner>
+      <Banner status="error">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+        pariatur, ipsum similique veniam.
+      </Banner>
+      <Card
+        title="Easy Deployment"
+        msg="Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis."
+      />
+      <TestimonialNoPic
+        msg="“Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa sapiente alias molestiae. Numquam corrupti in laborum sed rerum et corporis.”"
+        name="May Andersons"
+        jobTitle="CTO"
+        company="Acme Corp"
+        src={Logo}
+      />
+      <Testimonial
+        msg="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna nulla vitae laoreet augue. Amet feugiat est integer dolor auctor adipiscing nunc urna, sit."
+        name="Master Chief"
+        jobTitle="Officer"
+        company="Halo Project"
+        src="https://sm.ign.com/ign_br/screenshot/default/halo-infinite_hgkc.jpg"
+      />
       <br />
       <Menu>
         <Menu.Button>Menu</Menu.Button>
